473979d3943c9f80358299dcbabe4afbbab7a3b7
Robby Ranshous <rranshous@gmail.com>
2017-07-30 15:28:21 -0400

    very slow

--- gridwork.rb
+++ gridwork.rb
@@ -1,18 +1,23 @@
 require 'shoes'
 
-FPS = 60
-WINDOW_WIDTH=900
-WINDOW_HEIGHT=1100
+FPS = 1
+WINDOW_WIDTH=100
+WINDOW_HEIGHT=100
 
-screen = Screen.new height: WINDOW_HEIGHT, width: WINDOW_WIDTH
-cellspace = Cellspace.new
-cellspace.populate screen.pixels
-canvas = nil
-painter = Painter.new canvas
+class Canvas
+  attr_accessor :shoe
 
-Shoes.app width: WINDOW_WIDTH, height: WINDOW_HEIGHT, :title => 'gridwork' do
-  screen.pixels.each do |pixel|
-    cellspace.at(pixel).each {|c| c.fill painter }
+  def initialize shoe
+    self.shoe = shoe
+    puts 'new canvas'
+  end
+
+  def paint locatable
+    print 'p'
+    shoe.fill shoe.red
+    shoe.oval left: locatable.x(shoe),
+              top: locatable.y(shoe),
+              radius: 1
   end
 end
 
@@ -21,6 +26,7 @@ class Cellspace
 
   def initialize
     self.cells = []
+    puts 'new cellspace'
   end
 
   def populate space
@@ -30,18 +36,26 @@ class Cellspace
   end
 
   def at locatable
-    cells.select {|c| c.overlap? locatable }
+    cells.find {|c| c.overlap? locatable }
   end
 end
 
 module Locatable
   def left= mag; @left = mag; end
   def left; @left; end
-  def right= mag; @right = mag; end
-  def right; @right; end
+  def up= mag; @up = mag; end
+  def up; @up; end
 
   def overlap? locatable
     left == locatable.left && up == locatable.up
+  end
+
+  def x(space)
+    (space.width / 2) + left
+  end
+
+  def y(space)
+    (space.height / 2) - up
   end
 end
 
@@ -55,15 +69,28 @@ module Paintable
   end
 end
 
+class Object
+  def empty?
+    false
+  end
+end
+
+class NilClass
+  def empty?
+    true
+  end
+end
+
 class Cell
   include Locatable
   include Paintable
 
-  attr_accessor content
+  attr_accessor :content
 
   def initialize opts
     self.left  = opts[:left]  if !opts[:left].nil?
-    self.right = opts[:right] if !opts[:right].nil?
+    self.up    = opts[:up]    if !opts[:up].nil?
+    puts 'new cell'
   end
 
   def visible?
@@ -75,7 +102,8 @@ class Pixel
   include Locatable
   def initialize opts
     self.left  = opts[:left]  if !opts[:left].nil?
-    self.right = opts[:right] if !opts[:right].nil?
+    self.up    = opts[:up]    if !opts[:up].nil?
+    puts 'new pixel'
   end
 end
 
@@ -84,36 +112,56 @@ class Painter
 
   def fill paintable
     if paintable.visible?
-      canvas.paint paintable.offset
-    else
-      canvas.clear paintable.offset
+      canvas.paint paintable
     end
   end
 end
 
 class Screen
-  attr_accessor :width, :height
+  attr_accessor :width, :height, :pixels
 
   def initialize opts
     self.height = opts[:height] if opts[:height]
     self.width  = opts[:width]  if opts[:width]
+    self.pixels = self.class.create_pixels self.height, self.width
+    puts 'new screen'
   end
 
-  def pixels
-    Enumerator.new do |yielder|
-      (-half_width..half_width).each do |x|
-        (-half_height..half_height).reverse.each do |y|
-          yielder.call Pixel.new(left: x, up: y)
-        end
+  def self.create_pixels width, height
+    pixels = []
+    half_width  = width / 2
+    half_height = height / 2
+    (-half_width..half_width).each do |w|
+      (-half_height..half_height).to_a.reverse.each do |h|
+        pixels << Pixel.new(left: w, up: h)
       end
     end
+    pixels
   end
+end
 
-  def half_width
-    width / 2
-  end
 
-  def half_height
-    height / 2
+screen = Screen.new height: WINDOW_HEIGHT, width: WINDOW_WIDTH
+puts "pixels: #{screen.pixels.to_a.length}"
+cellspace = Cellspace.new
+cellspace.populate screen.pixels
+puts "cells: #{cellspace.cells.length}"
+painter = Painter.new
+
+Shoes.app width: WINDOW_WIDTH, height: WINDOW_HEIGHT, :title => 'gridwork' do
+  painter.canvas = Canvas.new self
+  animate FPS do
+    begin
+      puts "start"
+      clear
+      screen.pixels.each do |pixel|
+        cell = cellspace.at(pixel)
+        cell.fill painter
+      end
+    rescue => ex
+      puts "EXO: #{ex}"
+      puts "  : #{ex.backtrace}"
+      raise
+    end
   end
 end
